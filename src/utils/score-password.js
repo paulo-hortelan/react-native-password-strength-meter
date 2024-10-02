27701c0f40edb900f5cab8716dfce31f9fa894e7
paulo-hortelan <paulo.ribeiro@aonet.com.br>
2024-10-02 15:57:30 -0300

    imp: score

--- src/utils/score-password.js
+++ src/utils/score-password.js
@@ -1,29 +1,9 @@
-/* eslint radix: ["error", "as-needed"] */
-import limitValue from './limit-value';
+import zxcvbn from "zxcvbn";
 
 function scorePassword(pass, minLength, limit, variations = {}) {
-  let score = 0;
-  let variationCount = 0;
-  const letters = {};
+  const result = zxcvbn(pass);
 
-  if (!pass || pass.length < minLength) {
-    return score;
-  }
-
-  /* Score unique letters until 5 repetitions */
-  for (let i = 0; i < pass.length; i += 1) {
-    letters[pass[i]] = (letters[pass[i]] || 0) + 1;
-    score += 5.0 / letters[pass[i]];
-  }
-
-  /* Score character variation */
-  Object.keys(variations).forEach((variation) => {
-    const variationCheck = variations[variation].test(pass);
-    variationCount += variationCheck === true ? 1 : 0;
-  });
-  score += (variationCount - 1) * 10;
-
-  return limitValue(score, 0, limit);
+  return (result.score + 1) * 10;
 }
 
 export default scorePassword;
